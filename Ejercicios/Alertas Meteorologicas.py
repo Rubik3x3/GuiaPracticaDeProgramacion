1df5d7c0b40fe4f92294fb666031dc98bcf0545a
Rubik3x3 <franco.f4629@gmail.com>
2023-03-29 15:26:05 -0300

    Alertas Meteorologicas & Area de comedor creados.

---
+++ Ejercicios/Alertas Meteorologicas.py
@@ -0,0 +1,17 @@
+"""Alertas meteorológicas: acá se deberá controlar ciertos valores y cantidades para dar
+alertas meteorológicas.
+
+Se debe ingresar una cantidad de temperaturas promedios durante cierta
+cantidad de días para prever que pasara en los días posteriores. Nos pasaron los
+siguientes datos y requerimientos sobre el módulo:
+
+ Se sabe que cuando una temperatura supera los 30 es alta, cuando está por
+debajo de 11 es baja y en el resto es templada.
+ El usuario deberá ingresar temperaturas hasta haber terminado sin ingresar
+previamente la cantidad. Pero saber que el máximo de días, que puede
+registrar, es de 11.
+ Si se registra que hubo 7 días o más con temperaturas altas, quiere decir que
+vendrá una ola de calor.
+ Si se registras que hubo 5 días o más con temperaturas bajas, quiere decir que
+vendrá una ola polar.
+ En caso que no ocurra ninguna de las dos tendremos un buen tiempo."""
---
+++ Ejercicios/Area de Comedor.py
@@ -0,0 +1,11 @@
+"""Área de comedor: Dentro de esta área se quiere hacer un control de la entrada y salida
+de productos como carne y verdura que entran una cierta cantidad de días. Dentro de
+la funcionalidad se informará cuando se hace una compra, se ingresará el consumo de
+comida y si se pudre algún alimento.
+Además de realizar las funciones, dichas anteriormente, al finalizar el ingreso
+de datos se debe informar la cantidad total, si es que sobro, de comida (carne y
+verdura).
+Pero también se necesita controlar que en ningún momento se quede sin
+alguna comida. En caso de suceder se debe informar emitiendo alertas de que se ha
+quedado sin algún tipo de comida y los valores nunca pueden ser negativos (como
+mínimo 0)."""
