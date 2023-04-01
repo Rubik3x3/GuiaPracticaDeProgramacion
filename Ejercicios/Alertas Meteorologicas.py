1e159c06da2e4ec0b477ceaf08358a46eccb99dc
Franco Talarico <franco.f4629@gmail.com>
2023-03-31 21:54:31 -0300

    Alertas Meteorologicas

--- Ejercicios/Alertas Meteorologicas.py
+++ Ejercicios/Alertas Meteorologicas.py
@@ -15,3 +15,36 @@ vendrá una ola de calor.
  Si se registras que hubo 5 días o más con temperaturas bajas, quiere decir que
 vendrá una ola polar.
  En caso que no ocurra ninguna de las dos tendremos un buen tiempo."""
+
+tempAltas = 0
+tempBajas = 0
+tempTempladas = 0
+
+temperaturas = [0,0,0,0,0,0,0,0,0,0,0]
+
+def defTemp(temperatura):
+    if temperatura > 30:
+        global tempAltas
+        tempAltas+= 1
+    elif temperatura < 11:
+        global tempBajas
+        tempBajas+=1
+    else:
+        global tempTempladas
+        tempTempladas+=1
+
+def defOla():
+    if tempAltas >= 7:
+        return "Vendra una ola de calor."
+    elif tempBajas >= 5:
+        return "Vendra una ola polar."
+    else:
+        return "Tendremos un buen tiempo."
+
+for i in range(11):
+    print("Ingrese la temperatura del día ",i+1,":",end="")
+    temperaturas[i] = int(input())
+    defTemp(temperaturas[i])
+
+print("\n",defOla(),"\n")
+print("Temperaturas altas: ",tempAltas,"\nTemperaturas bajas: ",tempBajas,"\nTemperaturas Templadas: ",tempTempladas)
