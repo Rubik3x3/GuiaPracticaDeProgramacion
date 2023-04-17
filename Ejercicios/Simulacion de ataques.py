762b26e9bb340eecf81a16936cbf359f88b5b044
Rubik3x3 <franco.f4629@gmail.com>
2023-04-17 13:58:01 -0300

    Simulacion de ataques: ataque 1 y 3

--- Ejercicios/Simulacion de ataques.py
+++ Ejercicios/Simulacion de ataques.py
@@ -5,53 +5,68 @@ La vida y armadura (de 10 a 30) del enemigo y el daño que hace su personaje es
 ingresado por el usuario. El gamer puede elegir entre algunos tipos de ataques hasta
 que el oponente cae.
 
- Ataque múltiple perforante. El gamer elige la cantidad de ataques juntos que
+-- Ataque múltiple perforante. El gamer elige la cantidad de ataques juntos que
 le hace al enemigo. El daño que se le hace no contemplará su armadura
 
- Ataque básico. Tener en cuenta que al daño que hace el personaje se le
+-- Ataque básico. Tener en cuenta que al daño que hace el personaje se le
 descuenta la armadura del oponente.
 
- Ataque múltiple especial. El gamer elige la cantidad de ataques juntos que le
+-- Ataque múltiple especial. El gamer elige la cantidad de ataques juntos que le
 hace al enemigo. Pero solo se le efectuará ese daño cuando su armadura sea
 distinta a 20.
 """
 
-def ataqueMultPerf():
+danioPersonaje = 0
+
+def ataqueMultPerf(danio):
 	ataques=int(input("Ingrese la cantidad de ataques: "))
+	danio*=ataques
+	return danio
 
 def ataqueBasico(danio,armadura):
 	danio -= armadura
 	return danio
 
-def ataqueMultEsp(armadura):
+def ataqueMultEsp(danio,armadura):
 	ataques=int(input("Ingrese la cantidad de ataques juntos: "))
 	if armadura != 20:
-		
+		danio *= ataques
+		return danio
+	else:
+		print("\nLa armadura es igual a 20 y no se puede efectuar el ataque.")
 
 vidaEnemigo = int(input("Ingrese la vida del enemigo [10-30]: "))
 armaduraEnemigo = int(input("Ingrese la armadura del enemigo [10-30]: "))
-danioPersonaje = int(input("Ingrese el daño que hace su personaje: "))
+iDanioPersonaje = int(input("Ingrese el daño que hace su personaje: "))
+
+danioPersonaje = iDanioPersonaje
 
 if vidaEnemigo >= 10 and vidaEnemigo <= 30 and armaduraEnemigo >= 10 and armaduraEnemigo <= 30:
 		pass
-	else:
-		print("Ingrese los datos válidos.")
-		vidaEnemigo = int(input("Ingrese la vida del enemigo [10-30]: "))
-		armaduraEnemigo = int(input("Ingrese la armadura del enemigo [10-30]: "))
-		danioPersonaje = int(input("Ingrese el daño que hace su personaje: "))
 else:
-	exit()
+	print("\nIngrese los datos válidos.")
+	vidaEnemigo = int(input("Ingrese la vida del enemigo [10-30]: "))
+	armaduraEnemigo = int(input("Ingrese la armadura del enemigo [10-30]: "))
+	danioPersonaje = int(input("Ingrese el daño que hace su personaje: "))
 
 while vidaEnemigo > 0:
-	ans = int(input("Ataques:\n[1] Ataque múltiple perforante\n[2] Ataque básico\n[3] Ataque múltiple especial\nAtaque: "))
+	ans = int(input("\nAtaques:\n[1] Ataque múltiple perforante\n[2] Ataque básico\n[3] Ataque múltiple especial\n\nAtaque: "))
 	
 	if ans == 1:
-		ataqueMultPerf()
+		danioPersonaje = ataqueMultPerf(danioPersonaje)
+		vidaEnemigo -= danioPersonaje
 	elif ans == 2:
 		danioPersonaje = ataqueBasico(danioPersonaje,armaduraEnemigo)
+		armaduraEnemigo -= danioPersonaje
+		if armaduraEnemigo < 0:
+			danioPersonaje += armaduraEnemigo
 	elif ans == 3:
-		ataqueMultEsp(armaduraEnemigo)
-
-	vidaEnemigo -= danioPersonaje
-
-	print("La vida del enemigo es: ",vidaEnemigo)
+		ataqueMultEsp(danioPersonaje,armaduraEnemigo)
+		armaduraEnemigo -= danioPersonaje
+		if armaduraEnemigo < 0:
+			danioPersonaje += armaduraEnemigo
+	print("\nLa armadura del enemigo es: ",armaduraEnemigo,"La vida del enemigo es: ",vidaEnemigo)
+	danioPersonaje = iDanioPersonaje
+	
+if vidaEnemigo <= 0:
+	print("La armadura del enemigo es: ",armaduraEnemigo,"La vida del enemigo es: 0\nEl enemigo murio.")
