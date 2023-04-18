e47eaabaa0c0d32032499c187fdb83932e928794
= <=>
2023-04-18 19:45:47 -0300

    Factorial (Calcular solo numeros mayores o iguales a 0)

--- Ejercicios/Pensando para resolver/68.py
+++ Ejercicios/Pensando para resolver/68.py
@@ -3,15 +3,19 @@
 num = int(input("Ingrese un número para calcular su factorial: "))
 
 factorial = 0
-
-if num != 1 and num != 0:
-    for i in range(num,1,-1):
-        if i == num:
-            factorial = num
-        else:
-            factorial *= i
+if num >= 0:
+    if num != 1 and num != 0:
+        for i in range(num,1,-1):
+            if i == num:
+                factorial = num
+            else:
+                factorial *= i
+    else:
+        factorial = 1
+    print(factorial)
 else:
-    factorial = 1
+    print("Ingrese un número mayor que 0.")
+    exit()
 
-print(factorial)
+
 
