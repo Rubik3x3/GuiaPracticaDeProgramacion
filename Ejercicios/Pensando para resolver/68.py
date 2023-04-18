0b4944a64b72824d20b5f14b4fd387a4ca072236
Franco Talarico <franco.f4629@gmail.com>
2023-04-18 19:42:11 -0300

    Calcular Factorial (pensando para resolver (68) terminado)

--- Ejercicios/Pensando para resolver/68.py
+++ Ejercicios/Pensando para resolver/68.py
@@ -1,3 +1,17 @@
 #Dado un numero devolver su factorial.
 
-numero=int(input("Ingrese un número para ver su factorial: "))
+num = int(input("Ingrese un número para calcular su factorial: "))
+
+factorial = 0
+
+if num != 1 and num != 0:
+    for i in range(num,1,-1):
+        if i == num:
+            factorial = num
+        else:
+            factorial *= i
+else:
+    factorial = 1
+
+print(factorial)
+
