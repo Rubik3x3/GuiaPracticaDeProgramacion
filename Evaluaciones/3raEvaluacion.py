d62d67119c3828038e5430c9bdcd2a58fc5020d3
Rubik3x3 <franco.f4629@gmail.com>
2023-04-28 09:58:43 -0300

    Creando 3ra Evaluacion

---
+++ Evaluaciones/3raEvaluacion.py
@@ -0,0 +1,14 @@
+# 2da Evaluación de Taller - Franco Talarico - 3º1 11/04/2023 - Nota: 4/5
+
+"""
+Sistemas de gestión de tormentas
+Se debe registrar la duración total y la intensidad (de 1 a 10) de una tormenta en 1 día.
+Luego de eso se debe informar:
+
+1) El nivel de la tormenta: Medio o Alto. Medio será cuando la duración esté entre 15 y 40 minutos y con intensidad entre 5 y 7. 
+Nivel alto con duración entre 40 y 60 minutos y con intensidad entre 6 y 10.
+
+2) Si se debe emitir una alerta. Esto ocurre cuando la intensidad es superior a 8.
+
+3) Porecentaje del día libre de tormenta.
+"""
